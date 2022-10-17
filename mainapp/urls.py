9d481a199ef0162f20d85dfb0b569cc14d791dc1
Fredrick Simi <fkemosi@gmail.com>
2022-10-17 15:07:57 +0300

    new changes

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
 from . import views
 
+app_name='mainapp'
 urlpatterns = [
     path('', views.home_view, name='home'),
+    path('about', views.about_view, name='about'),
+    path('history', views.history_view, name='history'),
     path('contact', views.contact_view, name='contact'),
-    path('portfolio', views.portfolio_view, name='portfolio')
+    path('portfolio', views.portfolio_view, name='portfolio'),
+    path('programs', views.programs_view, name='programs'),
+    path('edc-community', views.edc_community_view, name='edc-community'),
+    path('services', views.services_view, name='services'),
+    path('volunteer-application', views.volunteer_application_view, name='volunteer-application'),
 ]
